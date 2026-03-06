ba0b0f99476f20acbe74d07d8082985a5688d13b
Emelio <emmanuelphiri3@gmail.com>
2026-03-06 09:22:11 +0200

    Minor Fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.pos"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 4
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
